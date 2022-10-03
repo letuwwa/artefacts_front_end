d59437a4c580de7ee950302d7f4e0b1edd617b29
Litvinenka <letuwwa@gmail.com>
2022-10-03 18:48:35 +0400

    feat: conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,25 @@ import ArtefactsList from "./ArtefactsList";
 
 const Home = () => {
     const [artefacts, setArtefacts] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        fetch("http://localhost:8000/artefacts/artefact/")
+        setTimeout(() => {
+            fetch("http://localhost:8000/artefacts/artefact/")
             .then(res => {
                 return res.json();
             })
             .then(data => {
                 console.log(data);
                 setArtefacts(data);
+                setIsLoading(false);
             });
+        }, 1000)
     }, []);
 
     return (
     <div className="home">
+        { isLoading && <div>Loading...</div> }
         {artefacts && <ArtefactsList artefacts={artefacts} title="What we have found:"/>}
     </div>
   );
