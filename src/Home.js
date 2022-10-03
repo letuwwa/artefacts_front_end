94a76a296f17c1f0a585baa7c9a096ef0e0e5275
Litvinenka <letuwwa@gmail.com>
2022-10-03 19:06:03 +0400

    feat: errors catching

--- src/Home.js
+++ src/Home.js
@@ -4,23 +4,32 @@ import ArtefactsList from "./ArtefactsList";
 const Home = () => {
     const [artefacts, setArtefacts] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch("http://localhost:8000/artefacts/artefact/")
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                console.log(data);
-                setArtefacts(data);
-                setIsLoading(false);
-            });
-        }, 1000)
+                .then(res => {
+                    if(!res.ok) {
+                        throw Error("Could not fetch data for that resource");
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setArtefacts(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch((err) => {
+                    setIsLoading(false);
+                    setError(err.message);
+                });
+        }, 1000);
     }, []);
 
     return (
     <div className="home">
+        { error && <div>{ error }</div>}
         { isLoading && <div>Loading...</div> }
         {artefacts && <ArtefactsList artefacts={artefacts} title="What we have found:"/>}
     </div>
