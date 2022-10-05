5d9766733c28d68489c632ff36b730631b9a159d
Litvinenka <letuwwa@gmail.com>
2022-10-05 13:22:15 +0400

    feat: react router links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Artefacts</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Artefact</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Artefact</Link>
             </div>
         </nav>
      );
