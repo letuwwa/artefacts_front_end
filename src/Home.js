8aace2c0443ecb84947979faf3413b20285282fa
Litvinenka <letuwwa@gmail.com>
2022-10-06 22:09:06 +0400

    fix: url

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import ArtefactsList from "./ArtefactsList";
 
 
 const Home = () => {
-    const { fetchedData: artefacts, isLoading, error } = useFetch("http://localhost:8000/artefacts_api/artefacts")
+    const { fetchedData: artefacts, isLoading, error } = useFetch("http://localhost:8000/artefacts_api/artefacts/")
 
     return (
     <div className="home">
