1c659d1eaea5d78748a92b852d882ec63ed30d28
Litvinenka <letuwwa@gmail.com>
2022-10-07 18:53:33 +0400

    feat: create form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
 const Create = () => {
+const [name, setName] = useState('');
+const [description, setDescription] = useState('');
+const [createdIn, setCreatedIn] = useState('');
+const [discoveredIn, setDiscoveredIn] = useState('');
+
     return ( 
         <div className="create">
             <h2>Add New Artefact</h2>
+            <form>
+                <label>Artefact name:</label>
+                <input 
+                    type="text"
+                    required 
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <label>Artefact description:</label>
+                <textarea
+                    required
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+                <label>Discovered in:</label>
+                <input 
+                    type="number"
+                    required
+                    value={discoveredIn}
+                    onChange={(e) => setDiscoveredIn(e.target.value)}
+                />
+                <label>Created in:</label>
+                <input 
+                    type="number"
+                    required
+                    value={createdIn}
+                    onChange={(e) => setCreatedIn(e.target.value)} 
+                />
+                <button>Save Artefact</button>
+            </form>
         </div>
      );
 }
