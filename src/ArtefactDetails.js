7607251976e07aed93dd076b211442b060d2fd9d
Litvinenka <letuwwa@gmail.com>
2022-10-09 14:52:01 +0400

    feat: delete button

--- src/ArtefactDetails.js
+++ src/ArtefactDetails.js
@@ -1,9 +1,18 @@
 import useFetch from "./useFetch";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 const ArtefactDetails = () => {
     const { uuid } = useParams()
     const {fetchedData: artefact, isLoading, error} = useFetch("http://localhost:8000/artefacts_api/artefacts/" + uuid)
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch("http://localhost:8000/artefacts_api/artefacts/" + uuid, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
 
     return ( 
         <div className="artefact-details">
@@ -15,6 +24,7 @@ const ArtefactDetails = () => {
                      { artefact.archeologist && <p>Discovered by {artefact.archeologist.first_name}</p>}
                      { artefact.discovery_year && <div>date of discovery: {artefact.discovery_year}</div>}
                      { artefact.creation_year && <div>created at: {artefact.creation_year}</div>}
+                     <button onClick={handleClick}>Delete artefact</button>
                 </article>
             )}
         </div>
