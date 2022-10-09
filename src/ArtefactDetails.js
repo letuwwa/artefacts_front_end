8978eb99bf7e2e008571bc9f9c12d99ad6f63c0d
Litvinenka <letuwwa@gmail.com>
2022-10-09 12:57:58 +0400

    fix: fetched data

--- src/ArtefactDetails.js
+++ src/ArtefactDetails.js
@@ -12,7 +12,7 @@ const ArtefactDetails = () => {
             { artefact && (
                 <article>
                      <h2>{ artefact.name }</h2>
-                     { artefact.archeologist && <p>Discovered by {artefact.archeologist.name}</p>}
+                     { artefact.archeologist && <p>Discovered by {artefact.archeologist.first_name}</p>}
                      { artefact.discovery_year && <div>date of discovery: {artefact.discovery_year}</div>}
                      { artefact.creation_year && <div>created at: {artefact.creation_year}</div>}
                 </article>
