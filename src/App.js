2a2cef50093de32f111852b6d1a5cb9141a47bd4
Litvinenka <letuwwa@gmail.com>
2022-10-10 18:13:12 +0400

    feat: notfound page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import Create from './Create';
 import ArtefactDetails from './ArtefactDetails';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import NotFound from './NotFound';
 
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
           </Route>
           <Route path="/artefacts/:uuid">
             <ArtefactDetails />
+          </Route>
+          <Route path="*">
+            <NotFound/>
           </Route>
         </Switch>
       </div>
