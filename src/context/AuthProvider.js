a84fe87d5fb3664713a238e4a6d6066deb70b044
Litvinenka <letuwwa@gmail.com>
2022-10-13 22:25:59 +0400

    feat: updateToken

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,55 +1,84 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AuthContext from './AuthContext'
 import jwt_decode from "jwt-decode";
 import { useHistory } from 'react-router-dom';
 
 export const AuthProvider = ({ children }) => {
-    let [authTokens, setAuthTokens] = useState(
-        () => localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null
-    );
-    let [user, setUser] = useState(
-        () => localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null
-    );
+    let [authTokens, setAuthTokens] = useState(()=> localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
+    let [user, setUser] = useState(()=> localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null)
+    let [loading, setLoading] = useState(true);
 
-    const history = useHistory();
+    const history = useHistory()
 
-    let loginUser = async (e) => {
-        e.preventDefault();
-        const response = await fetch("http://localhost:8000/auth_api/token/", {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(
-                {
-                    'username': e.target.username.value, 
-                    'password': e.target.password.value
-                }
-            )
-        });
-        const data = await response.json()
-        if (response.status === 200) {
-            setAuthTokens(data);
-            console.log(authTokens);
-            setUser(jwt_decode(data.access));
-            localStorage.setItem('authTokens', JSON.stringify(data));
-            history.push('/');
-        } else {
-            alert('Something went wrong!');
+    let loginUser = async (e )=> {
+        e.preventDefault()
+        let response = await fetch('http://127.0.0.1:8000/auth_api/token/', {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({'username':e.target.username.value, 'password':e.target.password.value})
+        })
+        let data = await response.json()
+        if(response.status === 200){
+            setAuthTokens(data)
+            setUser(jwt_decode(data.access))
+            localStorage.setItem('authTokens', JSON.stringify(data))
+            history.push('/')
+        }else{
+            alert('Something went wrong!')
         }
     }
-    
-    const logoutUser = () => {
-        setAuthTokens(null);
-        setUser(null);
-        localStorage.removeItem('authTokens');
+
+    let logoutUser = () => {
+        setAuthTokens(null)
+        setUser(null)
+        localStorage.removeItem('authTokens')
         history.push('/login')
     }
 
-    const contextData = {
+    let updateToken = async ()=> {
+        console.log('I work!')
+        let response = await fetch('http://127.0.0.1:8000/auth_api/token/refresh/', {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({'refresh':authTokens?.refresh})
+        });
+        let data = await response.json()
+        if (response.status === 200){
+            setAuthTokens(data);
+            setUser(jwt_decode(data.access));
+            localStorage.setItem('authTokens', JSON.stringify(data));
+        }else{
+            logoutUser();
+        }
+        if(loading){
+            setLoading(false);
+        }
+    }
+
+    let contextData = {
         user:user,
+        authTokens:authTokens,
         loginUser:loginUser,
         logoutUser:logoutUser,
     }
-     
+
+
+    useEffect(()=> {
+        if(loading){
+            updateToken();
+        }
+        let fourMinutes = 1000 * 60 * 4
+        let interval =  setInterval(()=> {
+            if(authTokens){
+                updateToken();
+            }
+        }, fourMinutes)
+        return ()=> clearInterval(interval);
+    }, [authTokens, loading])
 
     return (
         <AuthContext.Provider value={contextData}>
