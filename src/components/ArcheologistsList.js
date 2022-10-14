58e3d9db3b09a9885de702b1d5224606b4320907
Litvinenka <letuwwa@gmail.com>
2022-10-14 19:33:49 +0400

    feat: test page

--- src/components/ArcheologistsList.js
+++ src/components/ArcheologistsList.js
@@ -1,4 +1,5 @@
 const ArcheologistsList = () => {
+    const { fetchedData: archeologists, isLoading, error } = useFetch("http://localhost:8000/artefacts_api/archeologists/")
     return ( 
         <div className="archeologist-preview">
             Hi!
