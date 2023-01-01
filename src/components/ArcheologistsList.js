3c24e97d0fe0d38c2aed3c76da7b6995b38c7a2d
Maksim Litvinenka <letuwwa@gmail.com>
2023-01-01 16:59:09 +0400

    fix: css modules

--- src/components/ArcheologistsList.js
+++ src/components/ArcheologistsList.js
@@ -1,9 +1,11 @@
+import styles from "./ArtefactPreview.module.css";
+
 const ArcheologistsList = ({ archeologists, title }) => {
   return (
-    <div className="artefac-list">
+    <div className="artefact-list">
       <h2>{title}</h2>
       {archeologists.map((archeologist) => (
-        <div className="artefac-preview" key={archeologist.id}>
+        <div className={styles.artefactPreview} key={archeologist.id}>
           <h2>{archeologist.first_name + " " + archeologist.surname}</h2>
         </div>
       ))}
--- src/components/ArtefactsList.js
+++ src/components/ArtefactsList.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import styles from "./ArtefactPreview.module.css";
 
 const ArtefactsList = ({ artefacts, title }) => {
   return (
-    <div className="artefac-list">
+    <div className="artefact-list">
       <h2>{title}</h2>
       {artefacts.map((artefact) => (
-        <div className="artefac-preview" key={artefact.id}>
+        <div className={styles.artefactPreview} key={artefact.id}>
           <Link to={`/artefacts/${artefact.id}`}>
             <h2>{artefact.name}</h2>
           </Link>
